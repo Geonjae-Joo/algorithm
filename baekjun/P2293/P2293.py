29088934bd677aa5ec1f79195aa93d2963e2d301
Geonjae-Joo <binggraetogether@gmail.com>
2022-09-09 08:20:39 +0900

    master

--- baekjun/P2293/P2293.py
+++ baekjun/P2293/P2293.py
@@ -5,9 +5,27 @@ coins = [0]*100001
 max_coin = 0
 
 
-for i in range(n):
-    n = int(input())
-    max_coin = max(max_coin,n)
-    coins[n]+=1
-coins = coins[:max_coin+1]
+def dfs(x,coinSum):
+    global ans
+    coinSum +=x
+    if(coinSum==k): ans+=1
+    elif(coinSum>k): return
+    vst[x]+=1
+    for i in range(len(coins)):
+        if(vst[i]>=coins[i] or coinSum+i>k): continue
+        dfs(i,coinSum)
+    vst[x]-=1
 
+    
+
+
+for i in range(n):
+    coin = int(input())
+    max_coin = max(max_coin,coin)
+    coins[coin] = k//coin
+coins = coins[:max_coin+1]
+print(coins)
+vst = [0]*len(coins)
+ans=0
+dfs(0,0)
+print(ans)
